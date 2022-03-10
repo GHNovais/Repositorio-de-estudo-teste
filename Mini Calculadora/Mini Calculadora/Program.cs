ba4a7511790b9202e5d6977b212c0131debd915a
GHNovais <guilherme.novaish@gmail.com>
2022-03-10 20:17:39 -0300

    Teste - Adicionando o primeiro if "operacao * = Multiplicação"

--- Mini Calculadora/Mini Calculadora/Program.cs
+++ Mini Calculadora/Mini Calculadora/Program.cs
@@ -11,7 +11,7 @@ namespace Mini_Calculadora
 
             Console.WriteLine("Mini Calculadora");
 
-            Console.WriteLine("\nInforme a operaçao: \n+ = Adição\n- = Subtração\n* = multiplicação\n/ = Divisão");
+            Console.WriteLine("\nInforme a operaçao: \n+ = Adição\n- = Subtração\n* = Multiplicação\n/ = Divisão");
             operacao = Convert.ToChar(Console.ReadLine());
 
             if (operacao == '+')
@@ -36,6 +36,19 @@ namespace Mini_Calculadora
                 num2 = Convert.ToInt32(Console.ReadLine());
 
                 soma = num1 - num2;
+
+                Console.WriteLine("Resultado: "+soma);
+                Console.ReadKey();
+            }
+            else if (operacao == '*')
+            {
+                Console.WriteLine("\nInforme o primeiro número:");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Informe o segundo número:");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                soma = num2 * num1;
 
                 Console.WriteLine("Resultado: "+soma);
                 Console.ReadKey();
