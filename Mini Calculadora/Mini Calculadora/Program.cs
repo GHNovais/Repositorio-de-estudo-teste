b1a9c27db77ede6ace984c54071026f8d73c212e
GHNovais <guilherme.novaish@gmail.com>
2022-03-09 21:26:53 -0300

    Teste - Adicionando o primeiro if "operacao + = Adição"

--- Mini Calculadora/Mini Calculadora/Program.cs
+++ Mini Calculadora/Mini Calculadora/Program.cs
@@ -14,6 +14,19 @@ namespace Mini_Calculadora
             Console.WriteLine("\nInforme a operaçao: \n+ = Adição\n- = Subtração\n* = multiplicação\n/ = Divisão");
             operacao = Convert.ToChar(Console.ReadLine());
 
+            if (operacao == '+')
+            {
+                Console.WriteLine("\nInforme o primeiro número:");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Informe o segundo número:");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                soma = num2 + num1;
+
+                Console.WriteLine("\nResultado: "+soma);
+                Console.ReadKey();
+            }
         }
     }
 }
