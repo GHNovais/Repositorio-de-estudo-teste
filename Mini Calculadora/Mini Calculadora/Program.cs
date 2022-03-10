198f7e97ca51e188e9e92e97bddc5cff9e4f5424
GHNovais <guilherme.novaish@gmail.com>
2022-03-09 21:20:40 -0300

    Teste - Adicionando (Convert.ToChar(Console.ReadLine());) para guardar a operação informada pela usuário

--- Mini Calculadora/Mini Calculadora/Program.cs
+++ Mini Calculadora/Mini Calculadora/Program.cs
@@ -12,7 +12,7 @@ namespace Mini_Calculadora
             Console.WriteLine("Mini Calculadora");
 
             Console.WriteLine("\nInforme a operaçao: \n+ = Adição\n- = Subtração\n* = multiplicação\n/ = Divisão");
-            Console.ReadLine();
+            Convert.ToChar(Console.ReadLine());
 
         }
     }
