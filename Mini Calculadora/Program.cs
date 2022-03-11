609acb71f828e2cb8f26e576045f73ed4afc6dc7
GHNovais <guilherme.novaish@gmail.com>
2022-03-11 20:43:43 -0300

    Teste - Adicionando a operacao "+" utilizando o if

--- Mini Calculadora/Program.cs
+++ Mini Calculadora/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 namespace Mini_Calculadora
-{       
+{
     class Program
     {
         static void Main(string[] args)
@@ -13,6 +13,20 @@ namespace Mini_Calculadora
 
             Console.WriteLine("\nInforme a operaçao: \n+ = Adição\n- = Subtração\n* = Multiplicação\n/ = Divisão");
             operacao = Convert.ToChar(Console.ReadLine());
+
+            if (operacao == '+')
+            {
+                Console.WriteLine("\nInforme o primeiro número:");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Informe o segundo número:");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                soma = num1 + num2;
+
+                Console.WriteLine("\nResultado: " + soma);
+                Console.ReadKey();
+            }
         }
     }
 }
