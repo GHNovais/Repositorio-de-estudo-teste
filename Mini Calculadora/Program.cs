a667df9eb51572dc79f5a597230335e8bc65e1de
GHNovais <guilherme.novaish@gmail.com>
2022-03-11 20:44:21 -0300

    Teste - Adicionando a operacao "-"

--- Mini Calculadora/Program.cs
+++ Mini Calculadora/Program.cs
@@ -27,6 +27,19 @@ namespace Mini_Calculadora
                 Console.WriteLine("\nResultado: " + soma);
                 Console.ReadKey();
             }
+            else if (operacao == '-')
+            {
+                Console.WriteLine("\nInforme o primeiro número:");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Informe o segundo número:");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                soma = num1 - num2;
+
+                Console.WriteLine("\nResultado: " + soma);
+                Console.ReadKey();
+            }
         }
     }
 }
