800161dd22901d13a63adca863c725b3368512e7
GHNovais <guilherme.novaish@gmail.com>
2022-03-11 20:41:29 -0300

    Teste - Adicionado as operacoes disponiveis para o usuario

--- Mini Calculadora/Program.cs
+++ Mini Calculadora/Program.cs
@@ -8,6 +8,11 @@ namespace Mini_Calculadora
         {
             char operacao;
             int num1, num2, soma;
+
+            Console.WriteLine("Mini Calculadora");
+
+            Console.WriteLine("\nInforme a operaçao: \n+ = Adição\n- = Subtração\n* = Multiplicação\n/ = Divisão");
+            operacao = Convert.ToChar(Console.ReadLine());
         }
     }
 }
